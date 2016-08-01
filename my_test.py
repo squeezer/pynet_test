69ecff9f0924e9c9604b575fdb4c54ecd03385d2
Mike Queisser <mqueisser@box.com>
2016-08-01 13:33:07 -0700

    Added my_test, added logic to display 3 names, prompt for a 4th name, then display all four names

---
+++ my_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+name1 = 'Paul'
+name2 = 'George'
+name3 = 'Ringo'
+
+print "{:>30} {:>30} {:>30}".format(name1, name2, name3)
+
+name4 = raw_input('Please enter a fourth name: ')
+
+print "{:>30} {:>30} {:>30} {:>30}".format(name1, name2, name3, name4)
+
