68e1e83d6c3055da8df23b1920a19b7fff8eee44
Mike Queisser <mqueisser@box.com>
2016-08-02 10:08:41 -0700

    Exercise 14

---
+++ day2/my_test14.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+print('Exercise 14 - Dictionaries')
+
+network_device = {} # deliberately not using {'key': value} here 
+network_device['ip_address'] = '8.8.8.8'
+network_device['username'] = 'mq'
+network_device['password'] = 'passw0rd1'
+network_device['vendor'] = 'Palo Alto Networks'
+network_device['model'] = 'Widget 5000'
+
+
+for key, value in network_device.iteritems():
+    print key, value
+
+network_device['secret'] = 'Very secret'
+
+print(r'Checking device type...')
+device_type = network_device.get('device_type', 'cisco_ios')
+print device_type
